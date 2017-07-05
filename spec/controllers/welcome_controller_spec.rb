d20227e6d3597b3622aaaa5ed11f97c5dc50de26
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-05 16:22:43 +0300

    Add welcome controller and fill its spec with expectations

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+
+  describe 'GET #index' do
+    before { get :index }
+
+    it 'returns 200 status code' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it { is_expected.to render_with_layout :application }
+    it { is_expected.to render_template :index }
+  end
+end
+
