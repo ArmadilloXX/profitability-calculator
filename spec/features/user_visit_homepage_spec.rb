b443c268bb57e94d48002fb5c92079fbb3d0403e
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-05 15:55:24 +0300

    Add first expectation

--- spec/features/user_visit_homepage_spec.rb
+++ spec/features/user_visit_homepage_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.feature "UserVisitHomepage", type: :feature do
   context 'successfully' do
     before { visit root_path }
-    it 'shows welcome header'
+    it 'shows welcome header' do
+      expect(page).to have_content 'Welcome'
+    end
   end
 end
