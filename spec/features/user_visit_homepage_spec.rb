5e5427d098a1dd0b7c24bfd9d0a8ad1bbd682355
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-05 16:21:57 +0300

    Change expectation

--- spec/features/user_visit_homepage_spec.rb
+++ spec/features/user_visit_homepage_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "UserVisitHomepage", type: :feature do
   context 'successfully' do
     before { visit root_path }
     it 'shows welcome header' do
-      expect(page).to have_content 'Welcome'
+      expect(page).to have_css('h1', text: 'Welcome!')
     end
   end
 end
