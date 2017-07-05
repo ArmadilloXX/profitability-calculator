b5692b1f07728fe04bfc52c859eba66d162a6f6b
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-05 23:21:13 +0300

    Fulfill payments associations expectation

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,5 @@
 class Loan < ApplicationRecord
   belongs_to :borrower
+  has_many :payments
   validates :amount, presence: true
 end
--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Loan, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to :borrower }
+    it { is_expected.to have_many :payments }
   end
 
   describe 'validations' do
