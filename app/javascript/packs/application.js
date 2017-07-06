e97aaa93808167b3747c3c96776940fd887ebed3
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-06 13:02:35 +0300

    Fix HoundCI warnings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 /* eslint no-console:0 */
 
-import Vue from 'vue/dist/vue'
-import Buefy from 'buefy'
-import 'buefy/lib/buefy.css'
-import TurbolinksAdapter from 'vue-turbolinks'
-import Header from './components/Header.vue'
+import Vue from 'vue/dist/vue';
+import Buefy from 'buefy';
+import 'buefy/lib/buefy.css';
+import TurbolinksAdapter from 'vue-turbolinks';
+import Header from './components/Header.vue';
 
 Vue.component('application', {
   components: {
@@ -14,7 +14,7 @@ Vue.component('application', {
 
 document.addEventListener('turbolinks:load', () => {
   var element = document.getElementsByTagName('application')[0];
-  if (element != null) {
+  if (element !== null) {
     var app = new Vue({
       mixins: [TurbolinksAdapter],
       el: element
