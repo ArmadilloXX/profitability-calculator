3697c574682ab149ef6e486e2089e54acf55bc1e
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-06 16:39:40 +0300

    Add expectation to see loan payments table

--- spec/features/user_visit_loan_details_page_spec.rb
+++ spec/features/user_visit_loan_details_page_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'support/capybara'
 
 RSpec.feature 'User visit loan details page', type: :feature do
   let(:borrower)   { create :borrower }
@@ -19,4 +20,8 @@ RSpec.feature 'User visit loan details page', type: :feature do
   it "shows loan's loan plan name" do
     expect(page).to have_css('h2', text: loan_plan.name)
   end
+  it "shows loan's payments table", js: true do
+    expect(page).to have_css('table.table')
+  end
+
 end
