7a6dfaf88be7151f7b41ceec580224f76f32eae4
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-06 23:31:19 +0300

    Add route to payments index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/dashboard', to: 'dashboard#index', as: 'dashboard'
-  resources :loans, only: [:show]
+  resources :loans, only: [:show] do
+    resources :payments, only: [:index]
+  end
 end
