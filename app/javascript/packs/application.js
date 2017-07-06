56257cb3c19a0afa9d4ba7f1753d3f17d0ef3145
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-06 10:55:48 +0300

    Add Vue component to header and integrate it to the layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,13 @@ import Vue from 'vue/dist/vue'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
 import TurbolinksAdapter from 'vue-turbolinks'
+import Header from './components/Header.vue'
 
-Vue.component('application', {});
+Vue.component('application', {
+  components: {
+    appHeader: Header
+  }
+});
 
 document.addEventListener('turbolinks:load', () => {
   var element = document.getElementsByTagName('application')[0];
---
+++ app/javascript/packs/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        showMenu: false,
+      }
+    },
+    methods: {
+      toggleNavigation() {
+        this.showMenu = !this.showMenu;
+      },
+    }
+  }
+</script>
