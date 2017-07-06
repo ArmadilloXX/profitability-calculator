82f345bb2baa733185a1af0335bb564333fdafd4
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-06 16:35:36 +0300

    Fix Rubocop warnings

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -2,8 +2,9 @@ class LoansController < ApplicationController
   before_action :set_loan, only: [:show]
 
   def show; end
-  
+
   private
+
   def set_loan
     @loan = Loan.find(params[:id])
   end
--- spec/features/user_visit_loan_details_page_spec.rb
+++ spec/features/user_visit_loan_details_page_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.feature 'User visit loan details page', type: :feature do
   let(:borrower)   { create :borrower }
   let(:loan_plan)  { create :loan_plan }
-  let(:loan)       { create :loan, borrower: borrower, loan_plan: loan_plan}
+  let(:loan)       { create :loan, borrower: borrower, loan_plan: loan_plan }
 
   before { visit loan_path(loan) }
 
   it 'shows proper header' do
-    expect(page).to have_css('h1', text: "LOAN DETAILS")
+    expect(page).to have_css('h1', text: 'LOAN DETAILS')
   end
   it "shows loan's borrower name" do
     expect(page).to have_css('h2', text: borrower.name)
