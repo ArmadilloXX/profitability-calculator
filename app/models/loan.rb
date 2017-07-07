cdcbe5fe8396987b07a0ad67d0e6cc8dea462c5d
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-07 12:32:25 +0300

    Add dependent: :destroy option for payments in loan model

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,7 +1,7 @@
 class Loan < ApplicationRecord
   belongs_to :borrower
   belongs_to :loan_plan
-  has_many :payments
+  has_many :payments, dependent: :destroy
   validates :amount, presence: true
   validates :amount, numericality: { greater_than: 0 }
 end
