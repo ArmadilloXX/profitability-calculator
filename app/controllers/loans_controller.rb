6054f50df3b6bc45af104f924d94ef5c3b0477f7
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-07 11:09:34 +0300

    Add create action and loan_params

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -7,9 +7,22 @@ class LoansController < ApplicationController
 
   def show; end
 
+  def create
+    @loan = Loan.new(loan_params)
+    if @loan.save
+      render json: @loan, status: :created
+    else
+      render json: @loan.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_loan
     @loan = Loan.find(params[:id])
+  end
+
+  def loan_params
+    params.require(:loan).permit(:amount, :borrower_id, :loan_plan_id)
   end
 end
