75555b7eeb42f2e6218c454deaf436519d26a4c4
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-07 12:28:34 +0300

    Add feature spec for adding new payment

---
+++ spec/features/user_add_new_payment_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'support/capybara'
+
+RSpec.feature 'User add new payment', type: :feature, js: true do
+  let!(:borrower)   { create :borrower }
+  let!(:loan_plan)  { create :loan_plan }
+  let!(:loan)       { create :loan, loan_plan: loan_plan, borrower: borrower }
+
+  def fill_and_submit_new_payment_form
+    visit loan_path(loan)
+    click_on 'Add new payment'
+    fill_in 'payment[amount]', with: 191_666.67
+    select ,  from: 'payment[period]'
+    click_on 'Save'
+  end
+
+  context 'with correct data' do
+    before do
+      fill_and_submit_new_payment_form
+    end
+
+    it 'shows newly created payment amount in payments table' do
+      expect(page).to have_content(191_666.67)
+    end
+    it 'shows newly created payment period in payments table' do
+      expect(page).to have_content('Period 1')
+    end
+  end
+end
