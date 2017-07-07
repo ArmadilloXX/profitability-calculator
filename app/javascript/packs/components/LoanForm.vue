e5c84d95fe3504e9aed805469502e9665786c0f5
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-07 11:10:42 +0300

    Add loan form component to properly handle ajax form callbacks

---
+++ app/javascript/packs/components/LoanForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="new-loan-form" @ajax:error='showErrors' @ajax:success='handleSuccess'>
+    <h2 class="title is-3 has-text-centered">Add new loan</h2>
+    <br>
+    <b-field>
+      <div v-if='errors' class="form-errors">
+        {{ errors }}
+      </div>
+    </b-field>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        errors: undefined
+      }
+    },
+    methods: {
+      showErrors(event) {
+        this.errors = event.detail[0];
+      },
+      handleSuccess(event) {
+        Turbolinks.visit();
+      }
+    }
+  }
+</script>
+
+<style>
+ .form-errors {
+    padding: .5rem;
+    color: white;
+    background-color: hsl(348, 100%, 61%);
+  }
+</style>
