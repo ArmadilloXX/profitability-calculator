c34ebc39cf39fdb8fb3166ba03a25e5e4d966513
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-07 10:01:11 +0300

    Add parent component to wrap modal form and loans table

---
+++ app/javascript/packs/components/Loans.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-12">
+        <div class="level">
+          <div class="level-left">
+            <h2 class="title is-3">Loans</h2>
+          </div>
+          <div class="level-right">
+            <button class="button is-success is-large" @click='showNewLoanForm'>Add new loan</button>
+          </div>
+        </div>
+      </div>
+      <b-modal v-cloak :active.sync='newLoanFormOpened' :width='450'>
+        <div class="card">
+          <div class="card-content">
+            <slot name="loan-form"></slot>
+          </div>
+        </div>
+      </b-modal>
+    </div>
+    <app-loans-table></app-loans-table>
+  </div>
+</template>
+
+<script>
+  import LoansTable from './LoansTable.vue'
+  
+  export default {
+    data() {
+      return {
+        newLoanFormOpened: false
+      }
+    },
+    components: {
+      appLoansTable: LoansTable
+    },
+    methods: {
+      showNewLoanForm() {
+        this.newLoanFormOpened = true;
+      }
+    }
+  }
+</script>
