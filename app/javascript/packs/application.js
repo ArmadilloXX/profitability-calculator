a77ce98151b48453ffa9d5df6786d6421234c9e5
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-10 16:20:18 +0300

    Fix linting warnings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 /* eslint no-console:0 */
 
 import Vue from 'vue/dist/vue';
-import VueResource from 'vue-resource'
+import VueResource from 'vue-resource';
 import Buefy from 'buefy';
 import 'buefy/lib/buefy.css';
 import css from './stylesheets/application.sass';
@@ -30,9 +30,9 @@ Vue.component('application', {
 });
 
 document.addEventListener('turbolinks:load', () => {
-  var element = document.getElementsByTagName('application')[0];
+  let element = document.getElementsByTagName('application')[0];
   if (element !== null) {
-    var app = new Vue({
+    let app = new Vue({
       mixins: [TurbolinksAdapter],
       el: element
     });
