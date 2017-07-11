6ec5b6a186bf6a567ef5955de75adcacd00774a7
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-11 11:12:47 +0300

    Remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  # get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   post '/loans', to: 'loans#create', as: 'create_loan'
   resources :loans, only: [:index] do
     post '/payments', to: 'payments#create', as: 'create_payment'
