5da2e4f5925aface06927c3bb5a1b14d38169b6f
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-11 09:04:15 +0300

    Add Loan component with basic template

---
+++ app/javascript/packs/components/Loan.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="hero is-small is-primary">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title is-1">
+          Loan Details #{{ loanId }}
+        </h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loanId: this.$route.params.id
+      }
+    },
+  }
+</script>
