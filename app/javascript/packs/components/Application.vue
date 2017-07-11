63c6a1a9776ae95a0a037c66695388907493e17d
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-11 08:58:03 +0300

    Add main application component

---
+++ app/javascript/packs/components/Application.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <app-header></app-header>
+    <router-view></router-view>
+  </main>
+</template>
+
+<script>
+  import Header from './Header.vue';
+  import Home from './Home.vue';
+  import Loans from './Loans.vue';
+  import Loan from './Loan.vue';
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      appHeader: Header,
+      appHome: Home,
+      appLoans: Loans,
+      appLoan: Loan,
+    }
+  }
+</script>
