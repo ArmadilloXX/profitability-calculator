273a4eb832eb0fee586286cb9a3405bb78eb5315
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-11 09:05:34 +0300

    Remove unused routes. Add soft 404 handling to client side

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
+  # get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   post '/loans', to: 'loans#create', as: 'create_loan'
-  resources :loans, only: [:index, :show] do
+  resources :loans, only: [:index] do
     post '/payments', to: 'payments#create', as: 'create_payment'
     resources :payments, only: [:index]
   end
+  get '*any', to: 'welcome#index'
 end
