7b703ae76f68cb7a0ae7002c4c14b7ea814dae49
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-12 16:53:56 +0300

    Fix factories

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :payment do
     amount 191_666.67
+    sequence(:payment_period) { |n| "Month #{n}" }
+    overdue false
+    association :loan, factory: :loan
   end
 end
