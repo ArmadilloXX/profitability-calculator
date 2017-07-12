64dace6e65652d238c98a8fe4be6ecae56dd1a91
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-12 16:43:06 +0300

    Add exceptions handling concern

---
+++ app/controllers/concerns/exceptions_handler.rb
@@ -0,0 +1,12 @@
+module ExceptionsHandler
+  extend ActiveSupport::Concern
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, :not_found)
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e }, :unprocessable_entity)
+    end
+  end
+end
