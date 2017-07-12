6c7e90a8b764b9bae081f86cbc782cc04d477c54
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-12 16:47:38 +0300

    Add new actions to loans API controller

--- app/controllers/api/v1/loans_controller.rb
+++ app/controllers/api/v1/loans_controller.rb
@@ -2,16 +2,25 @@ class Api::V1::LoansController < Api::V1::ApiController
   before_action :set_loan, only: [:show]
 
   def index
-    render json: Loan.all
+    json_response Loan.all
   end
 
   def show
-    render json: @loan
+    json_response @loan
+  end
+
+  def create
+    @loan = Loan.create!(loan_params)
+    json_response @loan, :created
   end
 
   private
 
   def set_loan
     @loan = Loan.find(params[:id])
+  end
+
+  def loan_params
+    params.require(:loan).permit(:amount, :borrower_id, :loan_plan_id)
   end
 end
