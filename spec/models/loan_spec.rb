867b6fb7ef61a0750a8800a5f0a3c37355b70689
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-12 16:54:39 +0300

    Add validations expectations

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Loan, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to :borrower }
-    it { is_expected.to have_many :payments }
+    it { is_expected.to have_many(:payments).dependent(:destroy) }
     it { is_expected.to belong_to :loan_plan }
     it { is_expected.to have_attributes(received_sum: 0.0) }
     it { is_expected.to have_attributes(expected_sum: 0.0) }
@@ -11,6 +11,7 @@ RSpec.describe Loan, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of :amount }
+    it { is_expected.to validate_presence_of :borrower }
     it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
   end
 end
