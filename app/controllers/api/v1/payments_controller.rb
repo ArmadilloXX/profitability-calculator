d4f4fede35885b8dc202d542546950b2cbe9743b
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-12 16:48:25 +0300

    Add new actions to payments API controller

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -2,12 +2,22 @@ class Api::V1::PaymentsController < Api::V1::ApiController
   before_action :set_loan
 
   def index
-    render json: @loan.payments
+    @payments = @loan.payments
+    json_response @payments
+  end
+
+  def create
+    @payment = @loan.payments.create!(payment_params)
+    json_response @payment, :created
   end
 
   private
 
   def set_loan
     @loan = Loan.find(params[:loan_id])
+  end
+
+  def payment_params
+    params.require(:payment).permit(:amount, :payment_period, :overdue)
   end
 end
