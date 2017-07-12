41a668e8892dc48a2877734430aa5f777f444f66
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-12 16:52:33 +0300

    Add routes to new API controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root 'welcome#index'
   namespace :api do
     namespace :v1 do
-      resources :loans, only: [:index, :show] do
-        resources :payments, only: [:index]
+      resources :loans, only: [:index, :show, :create] do
+        resources :payments, only: [:index, :create]
       end
+      resources :borrowers, only: [:index]
+      resources :loan_plans, only: [:index]
     end
   end
-  post '/loans', to: 'loans#create', as: 'create_loan'
   get '*any', to: 'welcome#index'
 end
