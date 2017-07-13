98eceb540b1e27a2bc50956616c10abe988cc051
Ilya Dolgirev <ilya.dolgirev@gmail.com>
2017-07-13 13:09:14 +0300

    Add and run migration to change payment_period type

---
+++ db/migrate/20170713061609_change_payment_period_in_payments.rb
@@ -0,0 +1,6 @@
+class ChangePaymentPeriodInPayments < ActiveRecord::Migration[5.1]
+  def change
+    change_column :payments, :payment_period,
+      'integer USING CAST(payment_period AS integer)'
+  end
+end
